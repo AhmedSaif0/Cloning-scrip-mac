cf20d4e9cc30f3f9002d225244ce185a94634cfb
Ahmed Saifaldeen <71180230+AhmedSaif0@users.noreply.github.com>
2024-04-16 15:34:43 +0400

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,33 +1,46 @@
 
 
-# sources and destinations drives
-    source_volumes=("/dev/disk3s2" "/dev/disk3s3" "/dev/disk3s5" "/dev/disk3s6" "/dev/disk3s7" "/dev/disk4")
-    destination_volumes=("/dev/disk5s2" "/dev/disk5s3" "/dev/disk5s4" "/dev/disk5s5" "/dev/disk5s6" "/dev/disk5s7")
+         source_volumes=("/dev/disk3s3" "/dev/disk3s4" "/dev/disk3s5" "/dev/disk3s6" "/dev/disk3s7" "/dev/disk5" "/dev/disk4")
+    destination_volumes=("/volumes/NewVolume1" "/volumes/NewVolume2" "/volumes/NewVolume3" "/volumes/NewVolume4" "/volumes/NewVolume5" "/volumes/NewVolume6" "/volumes/NewVolume7")
+   # destination_volumes=("/dev/disk6s2" "/dev/disk6s3" "/dev/disk6s4" "/dev/disk6s5" "/dev/disk6s6" "/dev/disk6s7" "/dev/disk6s8")
 
     source="/dev/disk3"
-    destination="disk5"
+    destination="disk6"
 
     # first step format dis as jhfs
-    sudo diskutil eraseDisk JHFS+ NewDrive disk5 
+    sudo diskutil eraseDisk JHFS+ NewDrive $destination
     sleep 4
 
-    totalSize=$(diskutil info disk5 | awk '/Disk Size/ {print $3}')
-    totalSizeFormatted=$(echo $totalSize | sed 's/^[^0-9]*//;s/[Bb]$//')
+    totalSize=$(diskutil info $destination | awk '/Disk Size/ {print $3}')
+    totalSizeFormatted=$(echo $totalSize | sed 's/^[^0-9]*//;s/[Bb]$//;s/\.[0-9]*$//')
+
+    if [ "$totalSizeFormatted" = "1" ]; then
+         totalSizeFormatted= 931
+    fi
 
     echo "                                                           "
-    echo "total size of destination volume : "$totalSizeFormatted" GB"
+    echo "total size of destination volume : "$totalSizeFormatted" TB"
     echo "                                                           "
 
-    quota=$((totalSizeFormatted / 7))
-    echo "each volume will be "$quota"GB"
+   quota=$((totalSizeFormatted / 7))
+   echo "each volume will be "$quota"GB"
     echo "                                                           "
 
-    diskutil splitPartition disk5s2 JHFS+ NewVolume1 $quota"g" JHFS+ NewVolume2 $quota"g"  JHFS+ NewVolume3 $quota"g" JHFS+ NewVolume4 $quota"g" JHFS+ NewVolume5 $quota"g" JHFS+ NewVolume6 $quota"g"
-echo "▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"
+   diskutil splitPartition disk6s2 JHFS+ NewVolume1 $quota"g" JHFS+ NewVolume2 $quota"g"  JHFS+ NewVolume3 $quota"g" JHFS+ NewVolume4 $quota"g" JHFS+ NewVolume5 $quota"g" JHFS+ NewVolume6 $quota"g"
+
+
+  # diskutil splitPartition disk6s2 JHFS+ NewVolume1 50g JHFS+ NewVolume2 22g  JHFS+ NewVolume3 30g JHFS+ NewVolume4 39g JHFS+ NewVolume5 39g JHFS+ NewVolume6 50g JHFS+ NewVolume7 670g
+
+    echo "▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"
     echo "                                                           "
-    echo "created 6 volumes completed successfully."
+    echo "created 7 volumes completed successfully."
     echo "                                                           "
-echo "▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"
+    echo "▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"
+
+    echo "==================================================================="
+    echo "this script created by Eng : Ahmed Saifeldeen for more information"
+    echo "visit http://ahmedsaif.netlify.app"
+    echo "==================================================================="
 
    sleep 3
 
@@ -37,23 +50,15 @@ restore_volume() {
     local destination_volume="$2"
 
     echo "Restoring from source volume: $source_volume to destination volume: $destination_volume"
-    sudo asr restore --source "$source_volume" --target "$destination_volume" --erase --noprompt
+     sudo asr restore --source "$source_volume" --target "$destination_volume"   --noverify --erase --noprompt
   
 
     sleep 2
 }
 
 # Loop through the volumes and perform the restoration
-for ((i=1; i<=${#source_volumes[@]}; i++)); do
+for ((i=0; i<=${#source_volumes[@]}; i++)); do
 
-    echo "Unmounting source volume: ${source_volumes[$i]}"
-    diskutil unmountDisk force "${source_volumes[$i]}"
-
-    
-    sleep 1
-    echo "Unmounting destination volume: ${destination_volumes[$i]}"
-    diskutil unmountDisk force "${destination_volumes[$i]}"
-   
     echo "                                                           "
     echo "███████████████████████████████████████████████████████████"
     echo "                                                           "
@@ -76,4 +81,5 @@ echo "                                                               "
     echo "Volume cloning and verification completed successfully."
 echo "                                                               "
 echo "▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"
+
 
